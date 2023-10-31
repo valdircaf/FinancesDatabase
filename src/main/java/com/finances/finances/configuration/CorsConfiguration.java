cda783714430a6c76aaf0b4dc16460d6523a2634
valdircaf <valdircaf@hotmail.com>
2023-10-31 13:55:57 -0300

    switch allowed websites

--- src/main/java/com/finances/finances/configuration/CorsConfiguration.java
+++ src/main/java/com/finances/finances/configuration/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("*")
+            .allowedOrigins("http://localhost:3000/", "https://finances-pn58jirln-valdircaf.vercel.app/")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
